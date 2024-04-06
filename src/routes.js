739dc67fae34d546da46be4177c05e11a1b7776a
misaelguedes <https://github.com/misaelguedes>
2024-04-06 11:29:19 -0300

    Criando a página Home

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Header from './components/header'
 import Menu from './components/menu'
+import Home from './pages/home'
 
 export default function RoutesApp() {
     return (
         <BrowserRouter>
             <Header/>
             <Menu/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
         </BrowserRouter>
     )
 }
