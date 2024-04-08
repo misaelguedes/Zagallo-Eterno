362362ac5b110c19cccf301b9d8039f47fa0b2b2
misaelguedes <https://github.com/misaelguedes>
2024-04-08 20:54:12 -0300

    Criando media query do Menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,14 +1,45 @@
 import './menu.css'
 
 import { Link } from "react-router-dom"
+import {useState, useEffect} from 'react'
 
 export default function Menu() {
+
+    const [itensVisible, setItensVisible] = useState(false)
+
+    useEffect(() => {
+        function mudouTamanho() {
+            setItensVisible(window.innerWidth >= 768)
+        }
+
+        window.addEventListener('resize', mudouTamanho);
+
+        mudouTamanho();
+
+        return () => {
+            window.removeEventListener('resize', mudouTamanho);
+          };
+    }, [])
+
+    function clickMenu() {
+        setItensVisible(!itensVisible)
+    }
+
+    function hideMenu() {
+        if (window.innerWidth <= 768) {
+            setItensVisible(false)
+        }
+    }
+
     return (
         <menu>
-            <div className="itens">
-                <Link to='/'>Home</Link>
-                <Link to='/historia'>História</Link>
-                <Link to='/titulos'>Títulos</Link>
+            <div className="itens" style={{ display: itensVisible ? 'block' : 'none' }}>
+                <Link onClick={hideMenu} to='/'>Home</Link>
+                <Link onClick={hideMenu} to='/historia'>História</Link>
+                <Link onClick={hideMenu} to='/titulos'>Títulos</Link>
+            </div>
+            <div className='burguer' onClick={clickMenu}>
+                <span>&equiv;</span>
             </div>
         </menu>
     )
